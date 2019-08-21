a3821e656f1eb14c6c4ea30503edf8b73966ebf2
marieotsuka <marieotsuka@users.noreply.github.com>
2019-08-21 08:24:55 -0400

    add shadow effects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
 $(function (){
 	console.log('script loaded');
 
+	$('h1').okshadow({
+		color: '#839389',
+		textShadow: true,
+		xMax: 5,
+		yMax: 5,
+		yOffset: 3,
+		fuzzMin: 0,
+		fuzzMax: 0,
+	});
+	$('#intro .button').okshadow({
+		color: '#839389',
+		xMax: 5,
+		yMax: 5,
+		yOffset: 3,
+		fuzzMin: 0,
+		fuzzMax: 0,
+	});
 });
