713a158fe945cb0836cb73cf8804f6e998794c20
marieotsuka <marieotsuka@users.noreply.github.com>
2019-10-10 11:24:14 -0400

    update sku packaging and copy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,42 @@
 $(function (){
 	console.log('script loaded');
 
-	$('h1').okshadow({
-		color: '#839389',
-		textShadow: true,
-		xMax: 5,
-		yMax: 5,
-		yOffset: 3,
-		fuzzMin: 0,
-		fuzzMax: 0,
+	// $('.textshadow').okshadow({
+	// 	textShadow: true
+	// 	// xMax: 5,
+	// 	// yMax: 5,
+	// 	// yOffset: 3,
+	// 	// fuzzMin: 1,
+	// 	// fuzzMax: 3,
+	// });
+	// $('#nibs').okshadow({
+	// 	color: 'rgb(145, 110, 85)',
+	// 	xMax: 15,
+	// 	yMax: 15
+	// });
+
+	$('#intro .shadow').okshadow({
+		color: 'rgb(145, 110, 85)'
 	});
-	$('#intro .button').okshadow({
-		color: '#839389',
-		xMax: 5,
-		yMax: 5,
-		yOffset: 3,
-		fuzzMin: 0,
-		fuzzMax: 0,
+
+	$('#goldenberry .shadow').okshadow({
+		color: 'rgb(148, 111, 84)'
 	});
+
+	
+	$('#banana .shadow').okshadow({
+		color: 'rgb(190, 121, 0)'
+	});
+
+
+	$('#mango .shadow').okshadow({
+		color: 'rgb(155, 82, 23)'
+	});
+
+
+	$('#pineapple .shadow').okshadow({
+		color: 'rgb(137, 115, 178)'
+	});
+
+	
 });
