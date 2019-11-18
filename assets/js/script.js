dfdc32c552f68a8cc3acb0c38cb94902657400de
marieotsuka <marieotsuka@users.noreply.github.com>
2019-11-17 23:14:52 -0500

    add mobile styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,11 @@
 $(function (){
 	console.log('script loaded');
+	$('.nutrition h4').click(function(){
+		$(this).next('.nutrition-table').slideToggle(150);
+	});
 
-
-	// $('#textshadow').realshadow({
-	// 	// inset: true,
-	// 	type: 'text',
-	// });
-
-	// $('#afternoon-after h2').realshadow({
-	// 	// inset: true,
-	// 	type: 'text',
-	// });
-
-	// // $('.button').realshadow({
-	// 	length: 3,
-	// 	inset: true,
-	// 	followMouse: false
-	// 	// angle: Math.PI / 4,
-	// });
-
-	// $('#goldenberry .shadow').realshadow({
-	// 	inset: true,
-	// 	color: 'rgb(148, 111, 84)'
-	// });
-
-	
-	// $('#banana .shadow').realshadow({
-	// 	inset: true,
-	// 	color: 'rgb(190, 121, 0)'
-	// });
-
-
-	// $('#mango .shadow').realshadow({
-	// 	inset: true,
-	// 	color: 'rgb(155, 82, 23)'
-	// });
-
-
-	// $('#pineapple .shadow').realshadow({
-	// 	inset: true,
-	// 	color: 'rgb(137, 115, 178)'
-	// });
-
+	$('.mobile-menu').click(function(){
+		$('ul.menu').slideToggle(150);
+	});
 	
 });
